8bd56dffdb96eb12da16e898d74108d5b2ce9ae1
Shobha gowle <reach2shobha11@gmail.com>
2025-09-17 09:02:03 -0700

    formatted

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ reporter: [['json', { outputFile: 'playwright-report.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
--- tests/myscript/myFirstPlaywrightScripts.spec.ts
+++ tests/myscript/myFirstPlaywrightScripts.spec.ts
@@ -4,133 +4,126 @@ import { gotoAndVerifyHomePage } from '../testHelpers/gotoAndVerifyHomePage';
 const HOME_URL = 'https://the-internet.herokuapp.com/';
 test('test', async ({ page }) => {
 
-   const items = await gotoAndVerifyHomePage(page, HOME_URL);
+    const items = await gotoAndVerifyHomePage(page, HOME_URL);
 
 
 
-    for(let i=0;i<items;i++)
-    {
-       const linkText=await page.locator('li').nth(i).textContent() ?? '';
+    for (let i = 0; i < items; i++) {
+        const linkText = await page.locator('li').nth(i).textContent() ?? '';
 
-       if(linkText==='Dynamic Controls'){
+        if (linkText === 'Dynamic Controls') {
 
-       const dynamicControl = page.getByRole('link', { name: 'Dynamic Controls' });
+            const dynamicControl = page.getByRole('link', { name: 'Dynamic Controls' });
             await dynamicControl.scrollIntoViewIfNeeded();
             await dynamicControl.click();
 
-             await expect(page).toHaveURL(`${HOME_URL}dynamic_controls`);
-            await expect(page.getByRole('heading',{ name : 'Dynamic Controls'})).toContainText('Dynamic Controls');
+            await expect(page).toHaveURL(`${HOME_URL}dynamic_controls`);
+            await expect(page.getByRole('heading', { name: 'Dynamic Controls' })).toContainText('Dynamic Controls');
             await expect(page.locator('p')).toContainText('This example demonstrates when elements (e.g., checkbox, input field, etc.) are changed asynchronously.');
-            await expect(page.getByRole('button',{name :'Remove'})).toContainText('Remove');
+            await expect(page.getByRole('button', { name: 'Remove' })).toContainText('Remove');
             await page.locator('input[type="checkbox"]').click();
-             await expect(page.locator('input[type="checkbox"]')).toBeChecked();
+            await expect(page.locator('input[type="checkbox"]')).toBeChecked();
 
-            
-          break;
-       }
+
+            break;
+        }
     }
 
 
 });
 
 
-test('verify javascript links',async({page}) =>
-    {
+test('verify javascript links', async ({ page }) => {
 
 
-const items= await gotoAndVerifyHomePage(page,HOME_URL);
-
-  
-    for(let i=0;i<items;i++){
+    const items = await gotoAndVerifyHomePage(page, HOME_URL);
 
 
-        const linkText=await page.locator('li').nth(i).textContent() ?? '';
+    for (let i = 0; i < items; i++) {
 
-     if(linkText==='JavaScript Alerts'){
 
-       const scriptAlert = page.getByRole('link', { name: 'JavaScript Alerts' });
+        const linkText = await page.locator('li').nth(i).textContent() ?? '';
+
+        if (linkText === 'JavaScript Alerts') {
+
+            const scriptAlert = page.getByRole('link', { name: 'JavaScript Alerts' });
             await scriptAlert.scrollIntoViewIfNeeded();
             await scriptAlert.click();
 
-             await expect(page).toHaveURL(`${HOME_URL}javascript_alerts`);
-            await expect(page.getByRole('heading',{ name : 'JavaScript Alerts'})).toContainText('JavaScript Alerts');
-            
+            await expect(page).toHaveURL(`${HOME_URL}javascript_alerts`);
+            await expect(page.getByRole('heading', { name: 'JavaScript Alerts' })).toContainText('JavaScript Alerts');
+
             //await expect(page.locator('p')).toContainText('Here are some examples of different JavaScript alerts which can be troublesome for automation');
-            const ite=await page.locator('li').count();
+            const ite = await page.locator('li').count();
             await expect(ite).toBe(3);
 
-            for(let i=0;i<ite;i++)
-                
-            {
+            for (let i = 0; i < ite; i++) {
 
-                 await expect(page.locator('button').nth(i)).toBeVisible();
-                 await expect(page.locator('button').nth(i)).toBeEnabled();
-                 // Explicitly wait for the button to be visible before clicking
-                 await page.locator('button').nth(i).waitFor({ state: 'visible', timeout: 10000 });
-                 console.log("button text is " + await page.locator('button').nth(i).textContent());
-                 await page.locator('button').nth(i).click();
-                 // Listen for dialog and accept it
-                 page.once('dialog', async dialog => {
-                     await dialog.accept();
-                 });
-                 
+                await expect(page.locator('button').nth(i)).toBeVisible();
+                await expect(page.locator('button').nth(i)).toBeEnabled();
+                // Explicitly wait for the button to be visible before clicking
+                await page.locator('button').nth(i).waitFor({ state: 'visible', timeout: 10000 });
+                console.log("button text is " + await page.locator('button').nth(i).textContent());
+                await page.locator('button').nth(i).click();
+                // Listen for dialog and accept it
+                page.once('dialog', async dialog => {
+                    await dialog.accept();
+                });
+
             }
-            
-            
 
-       }
-       break;
+
+
+        }
+        break;
     }
 
 });
 
-test('verify checkboxes',async({page}) =>
-    {
+test('verify checkboxes', async ({ page }) => {
 
 
-const items= await gotoAndVerifyHomePage(page,HOME_URL);
+    const items = await gotoAndVerifyHomePage(page, HOME_URL);
 
 
-    for(let i=0;i<items;i++){
+    for (let i = 0; i < items; i++) {
 
 
-        const linkText=await page.locator('li').nth(i).textContent() ?? '';
+        const linkText = await page.locator('li').nth(i).textContent() ?? '';
 
-     if(linkText==='Checkboxes'){
+        if (linkText === 'Checkboxes') {
 
-       const scriptAlert = page.getByRole('link', { name: 'Checkboxes' });
+            const scriptAlert = page.getByRole('link', { name: 'Checkboxes' });
             await scriptAlert.scrollIntoViewIfNeeded();
             await scriptAlert.click();
 
-             await expect(page).toHaveURL(`${HOME_URL}checkboxes`);
-            await expect(page.getByRole('heading',{ name : 'Checkboxes'})).toContainText('Checkboxes');
-            
+            await expect(page).toHaveURL(`${HOME_URL}checkboxes`);
+            await expect(page.getByRole('heading', { name: 'Checkboxes' })).toContainText('Checkboxes');
+
             //await expect(page.locator('p')).toContainText('Here are some examples of different JavaScript alerts which can be troublesome for automation');
-            const ite=await page.locator('input[type="checkbox"]').count();
+            const ite = await page.locator('input[type="checkbox"]').count();
             await expect(ite).toBe(2);
 
-            for(let i=0;i<ite;i++)
-                
-            {
+            for (let i = 0; i < ite; i++) {
 
-                 await expect(page.locator('input[type="checkbox"]').nth(i)).toBeVisible();
-                 await expect(page.locator('input[type="checkbox"]').nth(i)).toBeEnabled();
-                 // Explicitly wait for the button to be visible before clicking
-                 await page.locator('input[type="checkbox"]').nth(i).waitFor({ state: 'visible', timeout: 10000 });
-                 //console.log("button text is " + await page.locator('button').nth(i).textContent());
-                 await expect(page.locator('input[type="checkbox"]').nth(i)).not.toBeChecked();
-                 await page.locator('input[type="checkbox"]').nth(i).click();
-                  await expect(page.locator('input[type="checkbox"]').nth(i)).toBeChecked();
-                 
-                 
+                await expect(page.locator('input[type="checkbox"]').nth(i)).toBeVisible();
+                await expect(page.locator('input[type="checkbox"]').nth(i)).toBeEnabled();
+                // Explicitly wait for the button to be visible before clicking
+                await page.locator('input[type="checkbox"]').nth(i).waitFor({ state: 'visible', timeout: 10000 });
+                //console.log("button text is " + await page.locator('button').nth(i).textContent());
+                await expect(page.locator('input[type="checkbox"]').nth(i)).not.toBeChecked();
+                await page.locator('input[type="checkbox"]').nth(i).click();
+                await expect(page.locator('input[type="checkbox"]').nth(i)).toBeChecked();
+
+
             }
 
             await expect(page.locator('input[type="checkbox"]')).toBeChecked();
-            
-            
 
-       }
-       break;
+
+
+        }
+        break;
     }
 
 });
